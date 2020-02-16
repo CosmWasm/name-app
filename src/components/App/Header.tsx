918a3727b3e7cef93a46d3a06bbd3a6f324ce95c
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-02-16 09:13:03 +0100

    Header loads balance from blockchain

--- src/components/App/Header.tsx
+++ src/components/App/Header.tsx
@@ -1,20 +1,41 @@
 import MuiTypography from "@material-ui/core/Typography";
 import * as React from "react";
 
+import { types } from "@cosmwasm/sdk";
+
 import { useSdk } from "../../service";
 
 export interface HeaderProps {
     readonly children: any;
 }
 
+interface State {
+    readonly account?: types.CosmosSdkAccount;
+    readonly error?: string;
+}
+
+function renderAccount({coins}: types.CosmosSdkAccount): string {
+    return coins.map(({amount, denom}) => `${amount} ${denom}`).join(", ");
+}
+
 function Header(props: HeaderProps): JSX.Element {
-    const { address, getClient } = useSdk();
+    const { address, getAccount } = useSdk();
+
+    const [value, setValue] = React.useState<State>({});
+
+    const balance = value.account ? renderAccount(value.account) : (value.error ? value.error : "(Loading)");
+
+    // TODO: periodic updates somehow
+    React.useEffect(() => {
+        getAccount()
+            .then(account => setValue({account}))
+            .catch(err => setValue({error: `${err}`}));
+    }, [])
 
     return (
         <div>
             <MuiTypography color="secondary" variant="h6">Address: {address}</MuiTypography>
-            <span>Balance: unknown</span>
-
+            <span>Balance: {balance}</span>
             {props.children}
         </div>
     )
--- src/service/index.tsx
+++ src/service/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
 
-import { CosmWasmClient } from "@cosmwasm/sdk";
+import { CosmWasmClient, types, RestClient } from "@cosmwasm/sdk";
 
 import { burnerWallet, connect, Wallet } from "./sdk";
 
@@ -14,6 +14,8 @@ export interface ICosmWasmContext {
     readonly loading: boolean;
     readonly address: string;
     readonly getClient: () => CosmWasmClient;
+    // this should be a method on client, but for now we use RestClient...
+    readonly getAccount: () => Promise<types.CosmosSdkAccount>;
 }
 
 export const defaultConfig: AppConfig = {
@@ -24,6 +26,7 @@ const defaultContext: ICosmWasmContext = {
     loading: true,
     address: "",
     getClient: (): CosmWasmClient => { throw new Error("not yet initialized") },
+    getAccount: (): Promise<types.CosmosSdkAccount> => { throw new Error("not yet initialized") },
 };
 
 export const CosmWasmContext = React.createContext<ICosmWasmContext>(defaultContext);
@@ -59,13 +62,15 @@ export function SdkProvider(props: SdkProviderProps): JSX.Element {
     useEffect(() => {
         loadWallet()
             .then(wallet => connect(config.httpUrl, wallet))
-            .then(({address, client}) =>
+            .then(({address, client}) => {
+                const restClient = new RestClient(config.httpUrl);
                 setValue({
                     loading: false,
                     address: address,
                     getClient: () => client,
+                    getAccount: () => restClient.authAccounts(address).then(r => r.result.value),
                 })
-            ).catch(err => console.log(`Error: ${err}`));
+            }).catch(err => console.log(`Error: ${err}`));
 
         // TODO: return a clean-up function???
     }, [config.httpUrl, loadWallet]);
