3a7335cd60861780818bd2697355cbc2860e8062
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-02-22 00:34:40 +0100

    Use HashRouter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,20 @@
-import { createBrowserHistory } from "history";
 import React from "react";
-import { Route, Router, Switch } from "react-router";
+import { Route, Switch } from "react-router";
+import { HashRouter } from "react-router-dom";
 
 import { Header } from "../components/App";
 import ContractList from "../components/ContractList";
 import Details from "./details";
 
-export const history = createBrowserHistory();
-
 const Routes = (): JSX.Element => (
-  <Router history={history}>
+  <HashRouter basename="/" >
     <Header>
       <Switch>
         <Route exact path="/" component={ContractList} />
-        <Route exact path="/contract/:address" component={Details} />
+        <Route path="/contract/:address" component={Details} />
       </Switch>
     </Header>
-  </Router>
+  </HashRouter>
 );
 
 export default Routes;
