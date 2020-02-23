dfc6f7981813e34fb6aafe64fa912cee27d66ef9
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-02-23 20:56:32 +0100

    Add grid system

--- src/theme/PageLayout.tsx
+++ src/theme/PageLayout.tsx
@@ -1,14 +1,29 @@
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
 import * as React from "react";
+
+const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+});
 
 interface Props {
     readonly children: any;
 };
 
-// TODO: add any global styling here
+// TODO: figure out a better grid system
 export function PageLayout({children}: Props): JSX.Element {
+    const classes = useStyles();
+
     return (
-        <React.Fragment>
-            {children}
-        </React.Fragment>
+        <Grid className={classes.root} container spacing={3}>
+            <Grid item xs={2} />
+            <Grid item xs>
+                {children}
+            </Grid>
+            <Grid item xs={2} />
+        </Grid>
     );
 }
