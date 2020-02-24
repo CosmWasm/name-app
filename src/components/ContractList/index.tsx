fe34319b01beddfe50b99ca8b266c6749e09770f
Simon Warta <simon@warta.it>
2020-02-24 13:38:48 +0100

    Add type safety to `result` variable

--- src/components/ContractList/index.tsx
+++ src/components/ContractList/index.tsx
@@ -10,7 +10,7 @@ const defaultCodeId = config.codeId;
 
 // TODO: we need to fix rest api, so this is one call (currently the list by code id doesn't return addresses of the contracts)
 async function listContractsByCodeId(client: RestClient, codeId: number): Promise<readonly ContractItemProps[]> {
-    let result = [];
+    const result: ContractItemProps[] = [];
     const addrs = await client.listContractAddresses();
     for (const address of addrs) {
         const info = await client.getContractInfo(address);
