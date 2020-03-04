fc7c09bae504d179d77047a45426580cdc3a200c
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-03-04 09:42:14 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/name-app/precache-manifest.ce48bf149f822db7a8e9870278c369d7.js"
+  "/name-app/precache-manifest.b79751fefbbdf3d1ed306f77f8d15a43.js"
 );
 
 self.addEventListener('message', (event) => {
