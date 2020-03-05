be6533f3b4764e5e5c3a28b1922772d31422f680
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-03-05 20:49:09 +0100

    Replace search button with icon

--- src/components/ContractLogic/SearchForm.tsx
+++ src/components/ContractLogic/SearchForm.tsx
@@ -1,7 +1,9 @@
+import Icon from "@material-ui/core/Icon";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
 import { Form, Formik } from "formik";
 import * as React from "react";
 
-import { Button } from "../../theme";
 import { useBaseStyles } from "../../theme";
 import { FormValues } from "../Form";
 import { FormTextField } from "../Form/fields/FormTextField";
@@ -30,12 +32,20 @@ export const SearchForm: React.FC<SearchFormProps> = ({ handleSearch }: SearchFo
       {({ handleSubmit, isSubmitting }) => (
         <Form onSubmit={handleSubmit} className={`${classes.card} ${classes.form}`}>
           <div className={classes.input}>
-            <FormTextField placeholder="Name" name="name" type="text" />
-          </div>
-          <div>
-            <Button type="submit" disabled={isSubmitting}>
-              Search
-            </Button>
+            <FormTextField
+              placeholder="Name"
+              name="name"
+              type="text"
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Icon>
+                      <SearchIcon />
+                    </Icon>
+                  </InputAdornment>
+                ),
+              }}
+            />
           </div>
         </Form>
       )}
