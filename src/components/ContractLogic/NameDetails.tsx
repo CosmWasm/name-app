4724e8043e58818062e3319e3d76581beac8de4b
Ethan Frey <ethanfrey@users.noreply.github.com>
2020-03-06 13:39:13 +0100

    Disable Register button while processing

--- src/components/ContractLogic/NameDetails.tsx
+++ src/components/ContractLogic/NameDetails.tsx
@@ -64,6 +64,7 @@ export function NameDetails(props: NameDetailsProps): JSX.Element {
     /* eslint-disable-next-line @typescript-eslint/camelcase */
     const { purchase_price: purchasePrice } = props.contract;
     const payment = purchasePrice ? [purchasePrice] : undefined;
+    setState({ loading: true });
     try {
       await getClient().execute(
         contractAddress,
@@ -74,6 +75,7 @@ export function NameDetails(props: NameDetailsProps): JSX.Element {
       setState({ owner: address, loading: false });
       refreshAccount();
     } catch (err) {
+      setState({ loading: false });
       setError(err);
     }
     return true;
@@ -94,7 +96,7 @@ export function NameDetails(props: NameDetailsProps): JSX.Element {
       setState({ owner: newOwner, loading: false });
       refreshAccount();
     } catch (err) {
-      setState({ loading: false });
+      setState({ owner: address, loading: false });
       setError(err);
     }
   };
@@ -136,7 +138,7 @@ export function NameDetails(props: NameDetailsProps): JSX.Element {
         <br />
         Price: {printableCoin(props.contract.purchase_price)}
       </MuiTypography>
-      <Button color="primary" type="submit" onClick={doPurchase}>
+      <Button color="primary" type="submit" onClick={doPurchase} disabled={state.loading}>
         Register
       </Button>
     </div>
